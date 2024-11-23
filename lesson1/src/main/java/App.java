1d06313dfea0bf3b0308374607f67457d4c9e66b
TIMONIN <wow.creepok.ru@mail.ru>
2024-11-23 17:51:21 +0300

    fix PR

--- lesson1/src/main/java/App.java
+++ lesson1/src/main/java/App.java
@@ -2,8 +2,6 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Урок номер 1. Привет мир!");
-
         List<Product> products = List.of(
                 new Product(1, 5, 30, "Яблоко"),
                 new Product(2, 5, 20, "Груша"),
