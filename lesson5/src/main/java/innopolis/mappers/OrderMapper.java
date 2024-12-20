81e842cdf6bd8376b7144694196a58f332531712
TIMONIN <wow.creepok.ru@mail.ru>
2024-12-20 21:25:31 +0300

    fix homework

--- lesson5/src/main/java/innopolis/mappers/OrderMapper.java
+++ lesson5/src/main/java/innopolis/mappers/OrderMapper.java
@@ -10,7 +10,7 @@ public class OrderMapper implements RowMapper<Order> {
     @Override
     public Order mapRow(ResultSet rs, int rowNum) throws SQLException {
         return Order.builder()
-                .id(rs.getInt(rs.getInt("id")))
+                .id(rs.getInt("id"))
                 .costOfWork(rs.getInt("cost_of_work"))
                 .carId(rs.getInt("car_id"))
                 .clientId(rs.getInt("client_id"))
