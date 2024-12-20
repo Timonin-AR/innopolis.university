5896bd0994ee5c05a8749b7e5c60e839b4ae04a0
TIMONIN <wow.creepok.ru@mail.ru>
2024-12-20 21:25:31 +0300

    fix homework

--- lesson5/src/main/java/innopolis/repository/impl/OrderRepositoryImpl.java
+++ lesson5/src/main/java/innopolis/repository/impl/OrderRepositoryImpl.java
@@ -1,16 +1,17 @@
 package innopolis.repository.impl;
 
-import innopolis.db.DatabaseService;
-import innopolis.entity.Client;
 import innopolis.entity.Order;
 import innopolis.mappers.OrderMapper;
 import innopolis.repository.OrderRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
+import static innopolis.config.DataBaseConfig.getPostgresqlDataSource;
+
 public class OrderRepositoryImpl implements OrderRepository {
 
-    private final DatabaseService db = new DatabaseService();
+    private final JdbcTemplate jdbcTemplate = new JdbcTemplate(getPostgresqlDataSource());
     private final OrderMapper orderMapper = new OrderMapper();
 
     private static final String FIND_ALL_ORDERS = "select * from \"tireService\".orders";
@@ -21,21 +22,21 @@ public class OrderRepositoryImpl implements OrderRepository {
 
     @Override
     public List<Order> findAllOrder() {
-        return db.template.query(FIND_ALL_ORDERS, orderMapper);
+        return jdbcTemplate.query(FIND_ALL_ORDERS, orderMapper);
     }
 
     @Override
-    public void addNewOrder(Order order) {
-        db.template.update(ADD_NEW_ORDER, order.getCostOfWork(), order.getCarId(), order.getClientId());
+    public void createOrder(Order order) {
+        jdbcTemplate.update(ADD_NEW_ORDER, order.getCostOfWork(), order.getCarId(), order.getClientId());
     }
 
     @Override
     public void deleteOrderById(Integer clientId) {
-        db.template.update(DELETE_ORDER_BY_CLIENT_ID, clientId);
+        jdbcTemplate.update(DELETE_ORDER_BY_CLIENT_ID, clientId);
     }
 
     @Override
     public void updateCost(Integer clientId, Integer costOfWork) {
-        db.template.update(SET_COST_OF_WORK_BY_CLIENT_ID, costOfWork, clientId);
+        jdbcTemplate.update(SET_COST_OF_WORK_BY_CLIENT_ID, costOfWork, clientId);
     }
 }
