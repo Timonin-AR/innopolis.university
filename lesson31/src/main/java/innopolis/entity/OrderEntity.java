f20b58015d7e80da2bd3fe8bb9bc003bda6c04ed
TIMONIN <wow.creepok.ru@mail.ru>
2025-05-03 23:36:00 +0300

    JavaDoc

---
+++ lesson31/src/main/java/innopolis/entity/OrderEntity.java
@@ -0,0 +1,39 @@
+package innopolis.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+
+/**
+ * Класс для работы с сущностями заказ из базы данных
+ */
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "\"order\"")
+public class OrderEntity {
+    @Id
+    @Column
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private ClientEntity client;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private CarEntity car;
+
+    @Column
+    private Long price;
+
+    @Column
+    private String details;
+
+}
---
+++ lesson31/src/main/java/innopolis/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package innopolis.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Настройка параметров безопасности.
+ */
+@Configuration
+public class SecurityConfig {
+
+    /**
+     * Добавление пользователей и добавление ролей.
+     */
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        var user = User.withUsername("user")
+                .password("{noop}qwerty")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}qwerty")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    /**
+     * Метод для настройки доступов к endpoint для определенных ролей
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorization ->
+                        authorization
+                                .requestMatchers("/api/v1/**").hasRole("ADMIN")
+                                .requestMatchers("/swagger-ui/**").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/order/*").hasRole("USER"))
+                .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
